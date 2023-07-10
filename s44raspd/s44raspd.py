3b3fa3962115210f83ff410e2b888f17f64ce54a
tantanGH <tantangh68@gmail.com>
2023-07-10 21:28:24 +0900

    mcs wait time adjust

--- s44raspd/s44raspd.py
+++ s44raspd/s44raspd.py
@@ -162,7 +162,7 @@ def main():
     parser.add_argument("-o", "--oled", help="oled display", action='store_true')
     parser.add_argument("-d", "--port", help="serial device name", default='/dev/serial0')
     parser.add_argument("-s", "--baudrate", help="serial baud rate", type=int, default=38400)
-    parser.add_argument("-w", "--mcswait", help="wait msec for mcs data", type=int, default=500)
+    parser.add_argument("-w", "--mcswait", help="wait msec for mcs data", type=int, default=2000)
 
     args = parser.parse_args()
 
